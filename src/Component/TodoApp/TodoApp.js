3b07820e0424dea221455d1aabc4bf3e800e643f
Mohammed Junaid S <junaidshaheer1998@gmail.com>
2020-06-03 16:32:55 +0530

    delete function implemented

--- src/Component/TodoApp/TodoApp.js
+++ src/Component/TodoApp/TodoApp.js
@@ -15,17 +15,24 @@ storeItems =event =>{
     event.preventDefault();
     const { input }= this.state;
   
-    
     this.setState({
         items: [...this.state.items, input],
         input : ""
        
     });
 };
+deleteItem = key => {
+    const allItems = this.state.items;
+    allItems.splice(key, 1);
+
+    this.setState({
+        items: allItems
+    });
+};
+
     render() { 
         const{ input,items } = this.state;
-        console.log(items);
-
+       
         return (
             <div className="todo-container">
                  <form className="input-section" onSubmit={this.storeItems}>
@@ -37,7 +44,7 @@ storeItems =event =>{
                <ul>
                    {items.map((data, index) => (
                        <li key={index}>
-                           {data}  <i className="fas fa-trash-alt"></i>
+                            {data}  <i className="fas fa-trash-alt" onClick={() =>this.deleteItem(index)}></i>
                        </li>
                    )
                    )}
