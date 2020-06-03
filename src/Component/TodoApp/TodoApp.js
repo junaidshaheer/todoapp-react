1038eeae27f421a6b333facc14121c486a7d4862
Mohammed Junaid S <junaidshaheer1998@gmail.com>
2020-06-03 16:14:37 +0530

    Storing functionality added

--- src/Component/TodoApp/TodoApp.js
+++ src/Component/TodoApp/TodoApp.js
@@ -1,30 +1,47 @@
 import React, { Component } from 'react'
 import"./TodoApp.css";
 export default class TodoApp extends Component {
+    state = {
+        input: "",
+        items:[]
+    }
     handleChange = event =>{
         this.setState({
         input: event.target.value
 
     });
 };
+storeItems =event =>{
+    event.preventDefault();
+    const { input }= this.state;
+  
+    
+    this.setState({
+        items: [...this.state.items, input],
+        input : ""
+       
+    });
+};
     render() { 
-        const{ input } = this.state;
+        const{ input,items } = this.state;
+        console.log(items);
 
         return (
             <div className="todo-container">
-                 <form className="input-section">
+                 <form className="input-section" onSubmit={this.storeItems}>
                <h1>Todo App</h1> 
                <input type="text" value={input} onChange={this.handleChange} placeholder="Enter items..." />
 
                
                </form>
                <ul>
-                   <li>list <i className="fas fa-trash-alt"></i>
-                   </li>
-                   <li>list <i className="fas fa-trash-alt"></i>
-                   </li>
-                   <li>list <i className="fas fa-trash-alt"></i> 
-                   </li>
+                   {items.map((data, index) => (
+                       <li key={index}>
+                           {data}  <i className="fas fa-trash-alt"></i>
+                       </li>
+                   )
+                   )}
+                  
                </ul>
             </div> 
         );
